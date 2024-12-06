0f6f78f7705c04b587bf2ac3b92a37f93a635cf6
LenaPrelle <lenaprelle@web.de>
2024-12-06 11:52:24 +0100

    Add files via upload

---
+++ Kniffel/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Kniffel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel_wurf = new System.Windows.Forms.Panel();
+            this.label_wuerfe = new System.Windows.Forms.Label();
+            this.button_würfeln = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox_gezogene_zahlen = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.panel_wurf, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_wuerfe, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button_würfeln, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 3, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel_wurf
+            // 
+            this.panel_wurf.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_wurf.BackColor = System.Drawing.Color.White;
+            this.panel_wurf.Location = new System.Drawing.Point(43, 25);
+            this.panel_wurf.Name = "panel_wurf";
+            this.tableLayoutPanel1.SetRowSpan(this.panel_wurf, 2);
+            this.panel_wurf.Size = new System.Drawing.Size(354, 331);
+            this.panel_wurf.TabIndex = 0;
+            // 
+            // label_wuerfe
+            // 
+            this.label_wuerfe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_wuerfe.AutoSize = true;
+            this.label_wuerfe.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_wuerfe.Location = new System.Drawing.Point(443, 22);
+            this.label_wuerfe.Name = "label_wuerfe";
+            this.label_wuerfe.Size = new System.Drawing.Size(314, 67);
+            this.label_wuerfe.TabIndex = 1;
+            this.label_wuerfe.Text = "Würfe";
+            // 
+            // button_würfeln
+            // 
+            this.button_würfeln.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_würfeln.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_würfeln.Location = new System.Drawing.Point(443, 384);
+            this.button_würfeln.Name = "button_würfeln";
+            this.button_würfeln.Size = new System.Drawing.Size(314, 39);
+            this.button_würfeln.TabIndex = 2;
+            this.button_würfeln.Text = "Würfeln";
+            this.button_würfeln.UseVisualStyleBackColor = true;
+            this.button_würfeln.Click += new System.EventHandler(this.button_würfeln_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.Controls.Add(this.listBox_gezogene_zahlen, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(443, 92);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(314, 264);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // listBox_gezogene_zahlen
+            // 
+            this.listBox_gezogene_zahlen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_gezogene_zahlen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox_gezogene_zahlen.FormattingEnabled = true;
+            this.listBox_gezogene_zahlen.ItemHeight = 25;
+            this.listBox_gezogene_zahlen.Location = new System.Drawing.Point(18, 16);
+            this.listBox_gezogene_zahlen.Name = "listBox_gezogene_zahlen";
+            this.listBox_gezogene_zahlen.Size = new System.Drawing.Size(276, 154);
+            this.listBox_gezogene_zahlen.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Kniffel";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel_wurf;
+        private System.Windows.Forms.Label label_wuerfe;
+        private System.Windows.Forms.Button button_würfeln;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ListBox listBox_gezogene_zahlen;
+    }
+}
+
---
+++ Kniffel/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kniffel
+{
+    public partial class Form1 : Form
+    {
+        private Random wuerfel = new Random();  //imaginärer Würfel wird erstellt
+
+        //Variablen für die Häufigkeit der Würfe
+        int eins = 0;   
+        int zwei = 0;
+        int drei = 0;
+        int vier = 0;
+        int fünf = 0;
+        int sechs = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private int holeZahl()  //Zufallzahl ermitteln
+        {
+            int wurf = wuerfel.Next(1,7);   //nur Zahlen von 1 bis 6
+            return wurf;    
+        }
+
+
+        private string holeZahltext(int zahl)   //Zufallzahl wird in string umgewandelt
+        {
+            string zufallszahl_string = "";
+
+            if (zahl == 1)
+            {
+                zufallszahl_string = "eins";
+            }
+
+            else if (zahl == 2)
+            {
+                zufallszahl_string = "zwei";
+            }
+
+            else if (zahl == 3)
+            {
+                zufallszahl_string = "drei";
+            }
+
+            else if (zahl == 4)
+            {
+                zufallszahl_string = "vier";
+            }
+
+            else if (zahl == 5)
+            {
+                zufallszahl_string = "fünf";
+            }
+
+            else
+            {
+                zufallszahl_string = "sechs";
+            }
+
+            return zufallszahl_string;
+        }
+
+
+        private void button_würfeln_Click(object sender, EventArgs e)   //wird beim Clicken des Buttons ausgeführt
+        {
+            int zufallszahl_int = holeZahl();   //Zufallszahl wird gezogen
+            zeigeListe(zufallszahl_int);    //Anzeige der Häufigkeiten, der Zahlen
+            anzeigeWurf(zufallszahl_int);   //Zeichnet die Punkte für die aktuelle Zahl
+        }
+
+
+        private void zeigeListe(int zahl)   //Häufigkeiten der Zahlen wird aktualisiert
+        {
+            listBox_gezogene_zahlen.Items.Clear();  //setzt listBox zurück
+
+            //Variablen der Anzahl werden hochgezählt
+            if (zahl == 1)
+            {
+                eins += 1;
+            }
+
+            else if (zahl == 2)
+            {
+                zwei += 1;
+            }
+
+            else if (zahl == 3)
+            {
+                drei += 1;
+            }
+
+            else if (zahl == 4)
+            {
+                vier += 1;
+            }
+
+            else if (zahl == 5)
+            {
+                fünf += 1;
+            }
+
+            else
+            {
+                sechs += 1;
+            }
+
+            string zufallszahl_string = holeZahltext(zahl);    //zufallszahl wird in string umgewandelt
+            listBox_gezogene_zahlen.Items.Add(zufallszahl_string + " gewürfelt");   //Konsolen Ausgabe
+            listBox_gezogene_zahlen.Items.Add("----------");
+            listBox_gezogene_zahlen.Items.Add("Eins = " + eins + " mal");
+            listBox_gezogene_zahlen.Items.Add("Zwei = " + zwei + " mal");
+            listBox_gezogene_zahlen.Items.Add("Drei = " + drei + " mal");            
+            listBox_gezogene_zahlen.Items.Add("Vier = " + vier + " mal");
+            listBox_gezogene_zahlen.Items.Add("Fünf = " + fünf + " mal");
+            listBox_gezogene_zahlen.Items.Add("Sechs = " + sechs + " mal");    
+            
+        }
+
+
+        private void anzeigeWurf(int zahl)  //Punkte des aktuellen Würfels zeichnen
+        {
+
+            panel_wurf.Refresh();   //Zeichnung wird zurückgesetzt
+
+            Graphics gr = panel_wurf.CreateGraphics();  //Rand des Würfels wird gezeichnet
+            Pen p = new Pen(Color.Gray, 3);
+            gr.DrawRectangle(p, 0, 0, panel_wurf.Width, panel_wurf.Height);
+
+            //je nach Zufallszahl werden unterschiedliche Punkte gezeichnet
+            if (zahl == 1)
+            {
+                mitte_Punkt();
+            }
+
+            else if (zahl == 2)
+            {
+                links_oben_Punkt();
+                rechts_unten_Punkt(); 
+            }
+
+            else if (zahl == 3)
+            {
+                links_oben_Punkt();
+                mitte_Punkt();
+                rechts_unten_Punkt();
+            }
+
+            else if (zahl == 4)
+            {
+                rechts_oben_Punkt();
+                links_oben_Punkt();
+                rechts_unten_Punkt();
+                links_unten_Punkt();
+            }
+
+            else if (zahl == 5)
+            {
+                rechts_oben_Punkt();
+                links_oben_Punkt();
+                mitte_Punkt();
+                rechts_unten_Punkt();
+                links_unten_Punkt();
+            }
+
+            else
+            {
+                rechts_oben_Punkt();
+                links_oben_Punkt();
+                rechts_mitte_Punkt();
+                links_mitte_Punkt();
+                rechts_unten_Punkt();
+                links_unten_Punkt();
+            }
+            
+        }
+
+        private void links_oben_Punkt()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width * 0.25) - (durchmesser / 2));
+            float y_position =  Convert.ToSingle((panel_wurf.Height * 0.25) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+
+        private void rechts_oben_Punkt()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width *0.75) - (durchmesser / 2));
+            float y_position = Convert.ToSingle((panel_wurf.Height * 0.25) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+
+        private void rechts_mitte_Punkt()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width * 0.25) - (durchmesser / 2));
+            float y_position = Convert.ToSingle((panel_wurf.Height * 0.5) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+
+        private void mitte_Punkt ()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width * 0.5) - (durchmesser / 2));
+            float y_position = Convert.ToSingle((panel_wurf.Height * 0.5) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+
+        private void links_mitte_Punkt()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width * 0.75) - (durchmesser / 2));
+            float y_position = Convert.ToSingle((panel_wurf.Height * 0.5) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+
+        private void rechts_unten_Punkt()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width * 0.75) - (durchmesser / 2));
+            float y_position = Convert.ToSingle((panel_wurf.Height * 0.75) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+
+        private void links_unten_Punkt()
+        {
+            Brush farbe = Brushes.Gray;
+            float durchmesser = panel_wurf.Height / 5;
+            float x_position = Convert.ToSingle((panel_wurf.Width * 0.25) - (durchmesser / 2));
+            float y_position = Convert.ToSingle((panel_wurf.Height * 0.75) - (durchmesser / 2));
+            Graphics gr = panel_wurf.CreateGraphics();
+            gr.FillEllipse(farbe, x_position, y_position, durchmesser, durchmesser);
+        }
+    }
+}
